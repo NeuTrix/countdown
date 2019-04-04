5068051e424d0b53cf985a91bfbcdc6a64df2e1f
NeuTrix <neutrix@brandgeneering.com>
2019-04-04 13:28:16 -0700

    Add moment

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,3 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
+import {
+  formatDate,
+  getCountdownParts
+} from './api'
