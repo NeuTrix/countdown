67db51ad329bdf918accaae74dc7abdb4544b32d
NeuTrix <neutrix@brandgeneering.com>
2019-04-04 14:11:58 -0700

    update event card

--- components/EventList.js
+++ components/EventList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FlatList, Text } from 'react-native';
+import EventCard from './EventCard';
 
 class EventList extends Component {
   constructor(props) {
@@ -17,7 +18,7 @@ class EventList extends Component {
     return (
       <FlatList
         data={ this.state.events }
-        renderItem={({ item }) => <Text>{ item.title }</Text>}
+        renderItem={({ item }) => <EventCard event={item}/>}
         keyExtractor={item => item.id}
       />
     );
