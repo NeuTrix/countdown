4e1a900f9576c9f2b0bb4187a560dace72c23457
NeuTrix <neutrix@brandgeneering.com>
2019-04-04 14:20:51 -0700

    update styling and elements

--- components/EventCard.js
+++ components/EventCard.js
@@ -7,18 +7,65 @@ import {
 } from './api'
 
 const styles = StyleSheet.create({
-  // card
-  // cardHeader
-  // date
-  // title
-})
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    padding: 10,
+    paddingTop: 10,
+    paddingBottom: 20,
+    margin: 10,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  cardHeader: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  date: {
+    fontWeight: '200',
+    fontSize: 15,
+    color: '#bdbdbd',
+    width: '30%',
+    textAlign: 'right',
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: '300',
+    marginLeft: 7,
+    textAlign: 'left',
+  },
+  counterContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+  },
+  counter: {
+    width: '25%',
+    flex: 1,
+  },
+  counterText: {
+    fontSize: 40,
+    textAlign: 'center',
+  },
+  counterLabel: {
+    fontSize: 13,
+    fontWeight: '100',
+    color: '#a3a3a3',
+    textAlign: 'center',
+    paddingTop: 0,
+  },
+});
 
 const propTypes = {
   event: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    date: PropTypes.instanceOf(Date)
+    date: PropTypes.string,
+    // date: PropTypes.instacnceOf(Date)
   }),
 }
+
 
 export default function EventCard({event}) {
 
