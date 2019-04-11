5e8eec036e205d3ed57533addc237ea1ea3622db
NeuTrix <neutrix@brandgeneering.com>
2019-04-10 18:29:03 -0700

    refactor inline styling for text input
    Add state to Form

--- components/EventForm.js
+++ components/EventForm.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react'
 import { View, StyleSheet, Text, TextInput, TouchableHighlight} from 'react-native'
 
 class EventForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      title: '',
+    }
+  }
   
   handleAddPress = () => {
     this.props.navigation.navigate('list')
   }
+
+  handleChangeTitle( value ) {
+    this.setState({ title: value });
+  }
+
 
   render() {
     return (
       <View style={{ flex: 1 }} >
         {/* <View > */}
         <View style={styles.fieldContainer} >
-          <Text style={{  color: 'white' }}> Test </Text>
+          <TextInput 
+            style={styles.text}
+            placeholder="Event"
+            spellCheck={false} 
+            onChangeText={(value) => this.handleChangeTitle(value)}
+            value={this.state.title}
+          />
         </View>
         <TouchableHighlight onPress={this.handleAddPress} >
-          <Text>Add</Text>
+          <Text>{this.state.title}</Text>
         </TouchableHighlight>
       </View>
     )
@@ -26,8 +43,13 @@ const styles = StyleSheet.create({
   fieldContainer: {
     marginTop: 20,
     marginBottom: 20,
-    backgroundColor: 'orangered', 
-    color: 'white',
+    backgroundColor: 'whitesmoke', 
+  },
+  text: {
+    color: 'red',
+    height: 40,
+    marginTop: 20,
+    marginBottom: 20,
   }
 })
 
